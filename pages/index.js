768c459379fb69ab06fe84b19ed3aa3a16665d33
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-03-21 14:26:34 -0400

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import ReactAudioPlayer from 'react-audio-player';
 import { useState,useEffect } from 'react'
 
 export default function Home() {
