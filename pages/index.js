8fbdda5aa98b1e7addc008c0ae495880ca756994
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-03-21 14:07:36 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import ReactAudioPlayer from 'react-audio-player';
 import { useState,useEffect } from 'react'
 
 export default function Home() {
   const [session_time_left,set_session_time_left] = useState("25:00")
-  const [break_time_left,set_break_time_left] = useState("25:00")
+  const [break_time_left,set_break_time_left] = useState("5:00")
 
   const [session_length, set_session_length] = useState(25)
   const [break_length, set_break_length] = useState(5)
@@ -57,6 +56,8 @@ export default function Home() {
   const start_session_countdown=(countDownDate)=>{
 
     if(!on_break){
+      console.log("IN start_session_countdown, NOT ON BREAK")
+
       set_interval(setInterval(function() {
         // Get today's date and time
         var now = new Date().getTime();
@@ -79,7 +80,7 @@ export default function Home() {
         }
       }, 1000))
     }else{
-      console.log("IN start_session_countdown, NOT ON BREAK")
+      console.log("ON BREAK")
       set_break_interval(setInterval(function() {
         // Get today's date and time
         var now = new Date().getTime();
@@ -89,11 +90,9 @@ export default function Home() {
         // Time calculations for days, hours, minutes and seconds
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = ((distance % (1000 * 60)) / 1000).toFixed(0);
-          console.log(minutes + " " + seconds)
         set_break_time_left( (minutes > 9 ? minutes : "0" + minutes) +":" + (seconds > 9 ? seconds : "0" + seconds) )
           
         set_distance(distance)
-          console.log(break_time_left)
         // If the count down is over, write some text 
         if (minutes <= 0 && seconds <=0 ) {
           clearInterval(break_interval)
